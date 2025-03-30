d78dae4f8ae9160309b0abe02c02b0eb669a9f33
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-03-30 16:57:06 +0530

    feat: add example usage

---
+++ example/user.hh
@@ -0,0 +1,36 @@
+#include "user.decl.h"
+
+#include "allGather.hh"
+
+class start : public CBase_start {
+private:
+  int n;
+  int k;
+  int x;
+  int y;
+  CProxy_simBox sim;
+  CProxy_AllGather AllGather_array;
+
+public:
+  start(CkArgMsg *msg);
+
+  void fini(int numDone);
+};
+
+class simBox : public CBase_simBox {
+private:
+  CProxy_start startProxy;
+  int k;
+  int n;
+  int x;
+  int y;
+  long int *data;
+  long int *result;
+
+public:
+  simBox(CProxy_start startProxy, int k, int n, int x, int y);
+
+  void begin(CProxy_AllGather AllGather_array);
+
+  void done(allGatherMsg *msg);
+};
