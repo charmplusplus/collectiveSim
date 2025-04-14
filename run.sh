1208d1ba7dae5d6707b990f0d32b8af6882e2b94
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-04-14 14:57:13 +0530

    fix: clean script to run benchmarks

--- run.sh
+++ run.sh
@@ -5,19 +5,19 @@ for bytes in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576;
     kilo_bytes=$(( bytes / 1024 ))
     echo "--------------------------------------------------"
     echo "Running benchmark for: $kilo_bytes KB"
-    
+
     data_value=$(( bytes / 4 ))
     sum=0
 
     # warp-up runs
     for run in {1..2}; do
-        output=$(./charmrun +p2 ./$1 "$data_value")
+        mpirun -n 2 ./$1 "$data_value" >/dev/null 2>&1
     done
 
     # Benchmark runs
     for run in {1..3}; do
-        output=$(./charmrun +p2 ./$1 "$data_value")
-        time=$(echo "$output" | grep "Time Taken:" | awk '{print $3}')
+	    time_line=$(mpirun -n 2 ./$1 "$data_value" 2>&1 | grep "Time Taken:")
+	    time=$(echo "$time_line" | awk '{print $3}')
         echo "    Extracted Time: $time seconds"
         sum=$(echo "$sum + $time" | bc -l)
     done
