6dbb310aac63635a3d4b3927e4c9af5b28b473b9
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-04-14 14:01:41 +0530

    fix: init bug in message

--- message/message.cc
+++ message/message.cc
@@ -1,16 +1,15 @@
 #include "message_sim.decl.h"
 
 #define NUM_SIM 100000
-int MSG_SIZE{};
 
 class start : public CBase_start {
 private:
-    int doneCount;
+    int doneCount{};
     double start_time;
 public:
     start(CkArgMsg *m) {
-        MSG_SIZE = atoi(m->argv[1]);
-        CProxy_sim simArray = CProxy_sim::ckNew(thisProxy, 2);
+        int MSG_SIZE = atoi(m->argv[1]);
+        CProxy_sim simArray = CProxy_sim::ckNew(thisProxy, MSG_SIZE, 2);
         start_time = CkWallTimer();
     }
 
@@ -28,10 +27,11 @@ public:
 class sim : public CBase_sim {
 private:
     CProxy_start startProxy;
-    int cnt = 0;
+    int cnt{};
     CkCallback cb;
+    int MSG_SIZE{};
 public:
-    sim(CProxy_start _startProxy) : startProxy(_startProxy) {
+    sim(CProxy_start _startProxy, int msgSize) : startProxy(_startProxy), MSG_SIZE(msgSize) {
         if(thisIndex == 0) {
             int data[MSG_SIZE];
             for(int i = 0; i < MSG_SIZE; i++) {
